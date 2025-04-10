415bcd0072de428ac48581b64b9f2b01635451ff
Benjamin <i74578@github.com>
2025-04-10 16:20:31 +0200

    Implemented doubly linked list instead of single

--- include/game_utils.h
+++ include/game_utils.h
@@ -12,16 +12,19 @@ char *convertCardToString(Card *card);
 // ### Linked List ###
 typedef struct Node {
     void *data;
+    struct Node *prevNode;
     struct Node *nextNode;
 } Node;
 
 typedef struct {
+    Node *tail;
     Node *head;
     size_t elementSize;
 } LinkedList;
 
 LinkedList* createList(size_t elementSize);
-void addNode(LinkedList* list, void* data);
+void addNodeToFront(LinkedList* list, void* data);
+void addNodeToBack(LinkedList* list, void* data);
 Node* getNode(LinkedList* list, int index);
 Node* getLastNode(LinkedList* list);
 void freeList(LinkedList* list);
